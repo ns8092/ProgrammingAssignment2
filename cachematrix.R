5111a472222ce8211e6773eae1523a7cfe9aef32
ns8092 <ns8092.att.com>
2014-07-25 11:16:37 -0400

    updating cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse  
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above 
+##If the inverse has already been calculated then the inverse from the cache is retrieved.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   ## Return a matrix that is the inverse of 'x'
+   i <- x$getinverse()  
+   if(!is.null(i)) {
+      print("getting cached data")
+      return(i)
+  }  
+  i <- solve(x$get())
+  x$setinverse(i)
+  i  
 }
